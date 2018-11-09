a7f34730b894df42a06afb00f4bb8c5de5af85f6
guozhenyu-tu <guozhenyu93@sina.com>
2018-11-09 11:57:53 +0800

    基础配置
    基础配置

---
+++ gulp/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    //服务器配置
+    serverDev: {
+
+        name: 'jiafu-Employee_training',//服务名称
+
+        root: 'build',//目录
+
+        //host: "192.168.120.151",//ip
+
+        port: 696,//端口
+
+        livereload: true//是否支持实时刷新
+
+    }
+
+};
